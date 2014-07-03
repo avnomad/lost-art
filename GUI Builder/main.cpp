9c062415001f22b1927f6e22f235df308c294031
Vaptistis Anogeianakis <nomad@cornercase.gr>
2014-07-04 01:52:31 +0300

    Created a repository for the GUI Builder prototype project.
    --HG--
    branch : initialization

---
+++ GUI Builder/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <Eigen/Dense>
+
+int main()
+{
+	cout << "Hello Wolrd!" << endl;
+
+	system("pause");
+	return 0;
+} // end function main
